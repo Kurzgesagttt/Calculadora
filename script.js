42c90146adc2612d679941f69cd5ba93874eabcf
Kurzgesagt <lucasmorenorodrigues@hotmail.com>
2024-07-18 18:04:30 -0300

    Calculadora feita, yt vid bro code

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-document.getElementById("display").innerHTML = "1231";
+const display = document.getElementById("display");
+
+function addToDisplay(input){
+  display.value += input;
+}
+
+function clearDisplay(){
+  display.value = "";
+}
+
+function calculate(){
+  try{ 
+  display.value = eval(display.value);//eval func calculates an expression, eval(2+2+1) = 5;
+  }
+  catch(error){
+    display.value = "Error";
+  }
+}
